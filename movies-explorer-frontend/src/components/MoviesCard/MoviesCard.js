3f7e8a3a3729fdbb632ac989fd8bd5132d0908d4
ElenaKuchueva <120306299+ElenaKuchueva@users.noreply.github.com>
2023-09-14 18:35:00 +0300

    испрввить недочетыпосле проверки

--- movies-explorer-frontend/src/components/MoviesCard/MoviesCard.js
+++ movies-explorer-frontend/src/components/MoviesCard/MoviesCard.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./MoviesCard.css";
 import card from "../../images/card.png";
 
 function MoviesCard() {
+  const location = useLocation();
+
   return (
     <li className="moviesCard">
       <img src={card} className="moviesCard__card" alt="Карточка фильма." />
       <ul className="moviesCard__block">
         <li className="moviesCard__discription">33 слова о дизайне</li>
         <li 
-        className="moviesCard__like  moviesCard__delate-movie"
+        className={`moviesCard__like ${location.pathname === "/saved-movies" ? "moviesCard__delate-movie" : "moviesCard__like_active"}`}
         // className="moviesCard__like moviesCard__like_active moviesCard__like_inactive moviesCard__delate-movie"
          />
       </ul>
